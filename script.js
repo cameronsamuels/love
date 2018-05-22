040d7ce4fbd9cc85b4d195e08be1d9a4062f7366
Cameron Samuels <me@cameronsamuels.com>
2018-05-21 20:33:42 -0500

    (issue)Fixed Android SMS

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-if (navigator.userAgent.match(/iPhone|iPad|iPod|Android/i)) document.querySelector("a").href = "sms:&body=I love you";
+if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) document.querySelector("a").href = "sms:&body=I love you";
+else if (navigator.userAgent.match(/Android/i)) document.querySelector("a").href = "sms:?body=I love you";
 else document.querySelector("a").href = "mailto:?subject=I love you&body=I love you";
